ee3c0559ef542a4a9ded5f4d0c3c88590cbd1258
Pallav Kumar <furinehigh@outlook.com>
2025-11-27 16:43:36 +0530

    added keyboard as well as mobile controls

--- src/scenes/UI.js
+++ src/scenes/UI.js
@@ -63,6 +63,43 @@ export default class UI extends Phaser.Scene {
     showCenterText(txt, duration=10000){
         this.centerText.setText(txt)
         this.centerText.alpha = 1
-        this.tweens.add({targets: this.centerText, alpha})
+        this.tweens.add({targets: this.centerText, alpha: 0, delay: duration-400,duration: 400})
+    }
+
+    createKeyboardRelay(){
+        this.input.keyboard.on('keydown', e=>{
+            const gameScene = this.scene.get('game')
+            if (!gameScene) return
+            
+            if (e.code == 'ArrowUp' || e.code == 'Space') gameScene.events.emit('ui:jump')
+            if (e.code == 'ArrowLeft') gameScene.events.emit('ui:left:down')
+            if (e.code == 'ArrowRight') gameScene.events.emit('ui:right:down')
+            if (e.code == 'ArrowDown') gameScene.events.emit('ui:down:start')
+
+            if (e.code == 'KeyP') this.togglePause()
+        })
+
+        this.input.keyboard.on('keyup', e=> {
+            const gameScene = this.scene.get('game')
+            if (!gameScene) return
+
+            if (e.code === 'ArrowLeft') gameScene.events.emit('ui:left:up')
+            if (e.code === 'ArrowRight') gameScene.events.emit('ui:right:up')
+            if (e.code === 'ArrowDown') gameScene.events.emit('ui:down:stop')
+        })
+    }
+
+    createMobileControls(){
+        if (!this.sys.game.device.input.touch) return
+
+        const padY = this.scale.height - 100
+
+        this.leftBtn = this.add.rectangle(80, padY, 120, 80, 0x000000, 0.25).setOrigin(0.5).setDepth(1000)
+            .setInterective().on('pointerdown', () => this.scene.get('game').events.emit('ui:left:down'))
+            .on('pointerup', () => this.scene.get('game').events.emit('ui:left:up'))
+            .on('pointerout', () => this.scene.get('game').events.emit('ui:left:up'))
+        this.add.text(80, padY, '◀', {font: '32px Arial', fill: '#fff'}).setOrigin(0.5).setDepth(1001)
+
+        this.jumbBtn = this.add.rectangle(this.scale.width - 140, padY, )
     }
 }
