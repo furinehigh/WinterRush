9596f17b407741a2ef47c01bf760f71137d0a3f6
Pallav Kumar <furinehigh@outlook.com>
2025-12-11 17:26:16 +0530

    added new images for the new theme

--- src/game.js
+++ src/game.js
@@ -1,20 +1,20 @@
 import Preload from './scenes/Preload.js'
 import StartScene from './scenes/StartScene.js'
-import Game from './scenes/Game.js'
+import GameScene from './scenes/GameScene.js'
 import GameOverScene from './scenes/GameOverScene.js'
 import HistoryScene from './scenes/HistoryScene.js'
-import UI from './scenes/UI.js'
+import UIScene from './scenes/UIScene.js'
 
 const config = {
     type: Phaser.AUTO,
     scale: {
-        mode: Phaser.Scale.RESIZE,
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: '100%',
-        height: '100%'
+        width: 720,
+        height: 1280
     },
     physics: { default: 'arcade', arcade: { debug: false }},
-    scene: [Preload, StartScene, Game, GameOverScene, HistoryScene, UI]
+    scene: [Preload, StartScene, GameScene, UIScene, GameOverScene]
 }
 
 new Phaser.Game(config)
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -3,26 +3,46 @@ export default class Preload extends Phaser.Scene {
 
     preload() {
         // background
-        this.load.image('bg', 'assets/bg/bg_still.jpg')
+        this.load.setPath('assets/')
 
-        this.load.spritesheet('player_run', 'assets/player/player.png', {
-            frameWidth: 50,
-            frameHeight: 64
+        this.load.image('bg_sky', 'bg/sky.png')
+        this.load.image('ground', 'bg/ground_texture.png')
+
+        this.load.spritesheet('player_ski', 'player/player_ski.png', {
+            frameWidht: 128, frameHeight: 128
         })
 
-        this.load.spritesheet('player_jump', 'assets/player/player_jump.png', {
-            frameWidth: 50,
-            frameHeight: 64
+        this.load.image('player_jump', 'player/player_jump.png')
+        this.load.image('player_duck', 'player/player_duck.png')
+
+        this.load.spritesheet('tree', 'obstacles/tree_shake.png')
+        this.load.image('rock', 'obstacles/rock.png')
+        this.load.image('log', 'obstacles/log_horizontal.png')
+
+        this.load.audio('bgm', 'audio/music_loop.mp3')
+        this.load.audio('sfx_jump', 'audio/jump.mp3')
+        this.load.audio('sfx_slide', 'audio/slide.mp3')
+        this.load.audio('sfx_crash', 'audio/crash.mp3')
+
+        const width = this.cameras.main.width
+        const height = this.cameras.main.height
+        const progressBar = this.add.graphics()
+        const progressBox = this.add.graphics()
+
+        progressBar.fillStyle(0x222222, 0.8)
+        progressBox.fillRect(width/2 - 160, height/2, 320, 50)
+
+        this.load.on('progress', (value) => {
+            progressBar.clear()
+            progressBar.fillStyle(0x00ffff)
+            progressBar.fillRect(width/2 - 150, height/2 + 10, 300 * value, 30)
         })
 
-        this.load.spritesheet('player_slide', 'assets/player/player_slide.png', {
-            frameWidth: 50,
-            frameHeight: 64
+        this.load.on('complete', () => {
+            progressBar.destroy()
+            progressBox.destroy()
+            this.scene.start('start')
         })
-
-        // obstacles
-        this.load.image('tree', 'assets/obstacles/tree.png')
-        this.load.image('rock', 'assets/obstacles/rock.png')
     }
 
     create() {
